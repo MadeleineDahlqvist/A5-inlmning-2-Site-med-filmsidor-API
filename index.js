8685f42d17b1efaa92cc37b97a6c9d2db991be06
Madeleine <madeleine.dahlqvist@lerniastudent.se>
2024-01-23 22:03:56 +0100

    Add JS, HTML files and CSS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import fetch from "node-fetch";
 
 const app = express();
 
@@ -8,9 +9,17 @@ app.set("view engine", "handlebars");
 app.set("views", "./templates");
 
 app.get("/", async (request, response) => {
-    response.render("home");
+    const resApi = await fetch ('https://plankton-app-xhkom.ondigitalocean.app/api/movies');
+    const payload = await resApi.json();
+    const movies = await payload.data;
+    response.render("home", {movies});
 });
-app.get("/movie", async (request, response) => {
-    response.render("movie");
+
+app.get("/movie/:movieId", async (request, response) => {
+    const movie = await fetch ('https://plankton-app-xhkom.ondigitalocean.app/api/movies/' + request.params.movieId)
+    const payload = await movie.json();
+    response.render("movie", {payload});
 });
-app.use(express.static('./static')).listen(5080)
+
+
+app.use("/static", express.static('./static')).listen(5080);
